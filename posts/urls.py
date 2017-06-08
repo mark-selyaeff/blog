2ca3cba5bfdd6ca8501b16d9c2dcb5961b1aabcc
-v <mark.selyaeff@yandex.ru>
2017-06-08 17:24:54 +0300

    Added search, Drafts, etc

--- posts/urls.py
+++ posts/urls.py
@@ -20,9 +20,9 @@ from .views import *
 
 urlpatterns = [
     url(r'^$', post_list, name='list'),
-    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^create/$', post_create, name='create'),
-    url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
-    url(r'^(?P<id>\d+)/delete/$', post_delete, name='delete')
+    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', post_delete, name='delete')
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.db.models import Q
 
 # Create your views here.
 
 def post_list(request):
-    queryset_list = Post.objects.all()
+    queryset_list = Post.objects.active()
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(Q(title__icontains=query)|
+                                             Q(content__icontains=query)|
+                                             Q(user__first_name__icontains=query)|
+                                             Q(user__last_name__icontains=query))
     paginator = Paginator(queryset_list, 3) # Show 25 contacts per page
     page_request_var = "page"
     page = request.GET.get(page_request_var)
@@ -28,17 +37,24 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 
-def post_detail(request, id):
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug):
+    instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff and not request.user.is_superuser:
+            raise Http404
     context = {
         "instance": instance
     }
     return render(request, "post_detail.html", context)
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    print('sdf')
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Successfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -50,8 +66,8 @@ def post_create(request):
     }
     return render(request, "post_form.html", context)
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -66,8 +82,8 @@ def post_update(request, id=None):
     }
     return render(request, "post_form.html", context)
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully deleted")
     return redirect('posts:list')
